6f2c789d57201298e333010fa16bd38ac22c4907
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-05-16 21:10:19 +0300

    add storage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App';
-import '../node_modules/modern-normalize/modern-normalize.css';
+import contacts from './data/contacts.json';
+import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App data={{ contacts }} />
   </React.StrictMode>
 );
